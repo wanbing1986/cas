020219554e7876c6caf52b3dc4ec1875e22aff1f
Timur Duehr <tduehr@gmail.com>
2019-02-11 13:22:30 -0600

    more dead chicken shaking

--- support/cas-server-support-couchbase-service-registry/src/test/java/org/apereo/cas/services/CouchbaseServiceRegistryTests.java
+++ support/cas-server-support-couchbase-service-registry/src/test/java/org/apereo/cas/services/CouchbaseServiceRegistryTests.java
@@ -5,10 +5,12 @@ import org.apereo.cas.couchbase.core.CouchbaseClientFactory;
 import org.apereo.cas.util.junit.EnabledIfContinuousIntegration;
 
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.api.parallel.ResourceLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,7 +35,9 @@ import org.springframework.context.event.EventListener;
     })
 @Tag("Couchbase")
 @EnabledIfContinuousIntegration
+@Execution(ExecutionMode.SAME_THREAD)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ResourceLock("Couchbase")
 public class CouchbaseServiceRegistryTests extends AbstractServiceRegistryTests {
 
     @Autowired
